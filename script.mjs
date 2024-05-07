979a74e0501ea5eba14201f1d1b3fdc5de5534bc
NiladriChatterje <radongtxgpu@gmail.com>
2024-05-07 20:21:38 +0530

    basic layout and skeleton to capture videoStream

--- script.mjs
+++ script.mjs
@@ -1,2 +1,3 @@
 const webcam = document.getElementById("webcamStream");
-const container = document.getElementById("container")
+const container = document.getElementById("container");
+const webCamBtn = document.getElementById("startCam");
